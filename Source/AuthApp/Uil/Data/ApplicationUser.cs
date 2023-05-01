8f8ec1e4409b9a2646842384b1b5ee337ce76d2d
Henrik <125497982+HenrikAlpha@users.noreply.github.com>
2023-05-01 21:37:18 +0200

    Deleted AuthApp to Change to a Monolith

--- Source/AuthApp/Uil/Data/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace BioBooker.AuthApp.Uil.Data.Migrations;
-
-public class ApplicationUser : IdentityUser
-{
-
-}
--- Source/AuthApp/Uil/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,8 +0,0 @@
-
-namespace BioBooker.AuthApp.Uil.Pages.Logout;
-
-public class LogoutOptions
-{
-    public static bool ShowLogoutPrompt = true;
-    public static bool AutomaticRedirectAfterSignOut = false;
-}
