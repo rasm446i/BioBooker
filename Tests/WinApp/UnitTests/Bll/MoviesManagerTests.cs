3e4873418494cad92411db9fd526c80caff83ccb
visti3012 <103989952+visti3012@users.noreply.github.com>
2023-05-09 14:13:15 +0200

    try

---
+++ Tests/WinApp/UnitTests/Bll/MoviesManagerTests.cs
@@ -0,0 +1,28 @@
+using System;
+using BioBooker.WinApp.Bll;
+using Xunit;
+
+namespace BioBooker.WinApp.UnitTests.Bll
+{
+    public class MoviesManagerTests
+    {
+
+        [Fact]
+
+        public void test1()
+        {
+
+        //Arrange
+
+        //Act
+
+        //Assert
+
+
+        }
+
+
+
+
+    }
+}
