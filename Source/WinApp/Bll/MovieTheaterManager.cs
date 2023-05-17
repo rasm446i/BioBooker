0fafd0516eef166b6e1842174f7da0908c26e348
Jens Christoffersen <103989888+Jens0910@users.noreply.github.com>
2023-05-17 11:55:37 +0200

    winapp documentation done

--- Source/WinApp/Bll/MovieTheaterManager.cs
+++ Source/WinApp/Bll/MovieTheaterManager.cs
@@ -15,18 +15,24 @@ namespace BioBooker.WinApp.Bll
         {
             _movieTheaterService = new MovieTheaterService();
         }
+
+        /// <summary>
+        /// Creates an auditorium with a list of seats and the name of the auditorium.
+        /// Afterwards it creates a new movie theater with the specified name and the newly created auditorium.
+        /// </summary>
+        /// <param name="movieTheaterName">The name of the movie theater to be created.</param>
+        /// <param name="seats">The list of seats for the auditorium.</param>
+        /// <param name="auditoriumName">The name of the auditorium within the movie theater.</param>
+        /// <returns>A task that holds a boolean indicating whether the movie theater was successfully created and inserted.</returns>
         public async Task<bool> CreateMovieTheaterAndInsertAsync(string movieTheaterName, List<Seat> seats, string auditoriumName)
         {
             bool wasInserted;
             try
             {
-                // Create a new auditorium using the provided seat list and auditorium name
                 Auditorium newAuditorium = CreateAuditorium(seats, auditoriumName);
-
-                // Create a new movie theater with the provided name and the created auditorium
                 MovieTheater newMovieTheater = CreateMovieTheater(movieTheaterName, newAuditorium);
 
-                // Insert the new movie theater into the database asynchronously
+                // Inserts the new movie theater into the database asynchronously
                 wasInserted = await InsertMovieTheaterAsync(newMovieTheater);
             }
             catch (Exception ex)
@@ -38,27 +44,55 @@ namespace BioBooker.WinApp.Bll
             return wasInserted;
         }
 
+        /// <summary>
+        /// Inserts a movie theater into the database asynchronously.
+        /// </summary>
+        /// <param name="movieTheater">The movie theater to be inserted.</param>
+        /// <returns>A task that holds a boolean indicating whether the movie theater was successfully inserted or not.</returns>
         public async Task<bool> InsertMovieTheaterAsync(MovieTheater movieTheater)
         {
             bool wasInserted = await _movieTheaterService.InsertMovieTheaterAsync(movieTheater);
 
             return wasInserted;
         }
+
+        /// <summary>
+        /// Creates a new auditorium with the specified seats and auditorium name.
+        /// </summary>
+        /// <param name="seats">The list of seats for the auditorium.</param>
+        /// <param name="auditoriumName">The name of the auditorium.</param>
+        /// <returns>The created auditorium.</returns>
         public Auditorium CreateAuditorium(List<Seat> seats, string auditoriumName)
         {
             return new Auditorium(seats, auditoriumName);
         }
 
+        /// <summary>
+        /// Creates a new movie theater with the specified name and auditorium.
+        /// </summary>
+        /// <param name="movieTheaterName">The name of the movie theater.</param>
+        /// <param name="newAuditorium">The auditorium to be added movie theater that is being created.</param>
+        /// <returns>The created movie theater.</returns>
         public MovieTheater CreateMovieTheater(string movieTheaterName, Auditorium newAuditorium)
         {
             return new MovieTheater(movieTheaterName, newAuditorium);
         }
 
+        /// <summary>
+        /// Retrieves a list of movie theaters from the database asynchronously.
+        /// </summary>
+        /// <returns>A task that holds a list of movie theaters.</returns>
         public async Task<List<MovieTheater>> GetMovieTheatersAsync()
         {
             return await _movieTheaterService.GetMovieTheatersAsync();
         }
 
+        /// <summary>
+        /// Adds an auditorium to a movie theater in the database asynchronously.
+        /// </summary>
+        /// <param name="movieTheaterId">The ID of the movie theater to which the auditorium will be added.</param>
+        /// <param name="newAuditorium">The auditorium to be added to the movie theater.</param>
+        /// <returns>A task that holds a boolean indicating whether the auditorium was successfully added to the movie theater or not.</returns>
         public async Task<bool> AddAuditoriumToMovieTheaterAsync(int movieTheaterId, Auditorium newAuditorium)
         {
             bool wasInserted = await _movieTheaterService.InsertAuditoriumToMovieTheaterAsync(movieTheaterId, newAuditorium);
