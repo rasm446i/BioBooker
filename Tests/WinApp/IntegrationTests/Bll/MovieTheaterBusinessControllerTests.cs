bf3a632faf8b94fad07dcfde812ae7342dd69061
rasmus3856 <103989794+rasmus3856@users.noreply.github.com>
2023-05-27 12:40:47 +0200

    BOOM UNIT TEST VIRK😎

--- Tests/WinApp/IntegrationTests/Bll/MovieTheaterBusinessControllerTests.cs
+++ Tests/WinApp/IntegrationTests/Bll/MovieTheaterBusinessControllerTests.cs
@@ -12,66 +12,8 @@ namespace BioBooker.WinApp.IntegrationTests.Bll
 {
     public class MovieTheaterBusinessControllerTests
     {
-        private readonly string _connectionString = "server=localhost; Database=BioBooker; integrated security=true;";
-        private readonly string _sqlScript = @"USE master;
-
-DROP DATABASE IF EXISTS BioBooker;
-
-
-        CREATE DATABASE BioBooker;
-
-        USE BioBooker;
-
-CREATE TABLE MovieTheaters(
-  Id INT IDENTITY(1, 1),
-  [Name] VARCHAR(30) NOT NULL,
-  CONSTRAINT[PK_MovieTheaterId] PRIMARY KEY(Id),
-  CONSTRAINT[UQ_Name] UNIQUE([Name])
-);
-
-        CREATE TABLE Auditorium(
-  AuditoriumId INT IDENTITY(1, 1),
-  MovieTheaterId INT REFERENCES MovieTheaters(Id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  [Name] VARCHAR(30),
-  CONSTRAINT[PK_AuditoriumId] PRIMARY KEY(AuditoriumId),
-  CONSTRAINT[UQ_Name_MovieTheaterId] UNIQUE([Name], MovieTheaterId)
-);
-
-        CREATE TABLE Seats(
-        IsAvailable BIT,
-        SeatRow INT,
-        SeatNumber INT,
-        AuditoriumId INT,
-  CONSTRAINT[CompositeKey_Seat] PRIMARY KEY (SeatRow, SeatNumber, AuditoriumId),
-  CONSTRAINT[FK_AuditoriumId] FOREIGN KEY(AuditoriumId) REFERENCES Auditorium(AuditoriumId)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-);
-     
-
-        CREATE TABLE Movies(
-          Id INT IDENTITY(1, 1) PRIMARY KEY,
-          Title NVARCHAR(255) NOT NULL,
-          Genre NVARCHAR(255) NOT NULL,
-          Actors NVARCHAR(255) NOT NULL,
-          Director NVARCHAR(255) NOT NULL,
-        
-          [Language] NVARCHAR(255) NOT NULL,
-        ReleaseYear DATE NOT NULL,
-  Subtitles BIT NOT NULL,
-  SubtitlesLanguage NVARCHAR(255) NOT NULL,
-  MPARating NVARCHAR(255) NOT NULL,
-  RuntimeMinutes INT NOT NULL,
-  PremierDate DATE NOT NULL
-);
-
-        CREATE TABLE Posters(
-          MovieId INT PRIMARY KEY FOREIGN KEY REFERENCES Movies(Id) ON DELETE CASCADE,
-          PosterTitle NVARCHAR(255) NOT NULL,
-          ImageData VARBINARY(MAX) NOT NULL
-);";
+        private readonly string _connectionString = "server=.\\SQLExpress; Database=BioBooker; integrated security=true;";
+        private readonly string _sqlScript = @"DELETE FROM MovieTheaters WHERE Name = 'MovieTheater'";
         private MovieTheaterBusinessController _movieTheaterBusinessController;
 
         public MovieTheaterBusinessControllerTests()
@@ -82,10 +24,10 @@ CREATE TABLE MovieTheaters(
         [Fact]
         public async Task CreateMovieTheaterAndInsertAsync_ReturnsTrue_WhenMovieTheaterHasBeenInserted()
         {
-            ResetDatabase();
 
+            ResetDatabase();
             //arrange
-            string movieTheaterName = "Movie Theater";
+            string movieTheaterName = "MovieTheater";
             string auditoriumName = "Auditorium 1";
             List<Seat> seats = new List<Seat>
             {
@@ -101,42 +43,20 @@ CREATE TABLE MovieTheaters(
             //assert
             Assert.True(result);
 
-           
+            ResetDatabase();
         }
 
-        [Fact]
-        public async Task AddAuditoriumToMovieTheaterAsync_ReturnsTrue_WhenAuditoriumHasBeenAddedToMovieTheater()
+        private void ResetDatabase()
         {
-            //arrange
-            int movieTheaterId = 1;
-            string auditoriumName = "Auditorium 2";
-            List<Seat> seats = new List<Seat>
             {
-            new Seat(1, 1),
-            new Seat(1, 2),
-            new Seat(2, 1),
-            new Seat(2, 2)
-            };
-            Auditorium newAuditorium = _movieTheaterBusinessController.CreateAuditorium(seats, auditoriumName);
-
-            //act
-            bool result = await _movieTheaterBusinessController.AddAuditoriumToMovieTheaterAsync(movieTheaterId, newAuditorium);
-
-            //assert
-            Assert.True(result);
-
-          
-        }
-
-
-        public void ResetDatabase()
-        {
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-                connection.Execute(_sqlScript);
-                connection.Close();
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    connection.Execute(_sqlScript);
+                    connection.Close();
+                }
             }
+
         }
     }
 }
